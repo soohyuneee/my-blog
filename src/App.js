db18fa4317a8f0e91916435f0de87ab9dc388afd
soohyuneee <tlstngus95@naver.com>
2023-03-04 22:08:14 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+function App() {
+	const [title, setTitle] = useState('');
+
+	return (
+		<div className="container">
+			<div className="mb-3">
+				<label className="form-label">Title</label>
+				<input className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} />
+			</div>
+			<button className="btn btn-primary">Post</button>
+		</div>
+	);
+}
+
+export default App;
