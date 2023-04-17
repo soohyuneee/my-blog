2663b8825d6abc8ac0105ddba1a9f112fec270f8
soohyuneee <tlstngus95@naver.com>
2023-04-17 19:24:17 +0900

    속성명 수정

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ function NavBar() {
 				</Link>
 				<ul className="navbar-nav">
 					<li className="nav-item">
-						<NavLink activeClassName="active" className="nav-link" aria-current="page" to="/blogs">
+						<NavLink activeclassname="active" className="nav-link" aria-current="page" to="/blogs">
 							Blogs
 						</NavLink>
 					</li>
