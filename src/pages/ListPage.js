2f6c82183018c1044e9a166d45bfecacf6cccfc5
soohyuneee <tlstngus95@naver.com>
2023-04-17 19:24:48 +0900

    get 요청 추가

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,4 +1,26 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
 function ListPage() {
-	return <div>ListPage</div>;
+	const [posts, setPosts] = useState([]);
+
+	const getPosts = () => {
+		axios.get('http://localhost:3001/posts').then((res) => {
+			setPosts(res.data);
+		});
+	};
+
+	useEffect(() => {
+		getPosts();
+	}, []);
+
+	return (
+		<div>
+			<h1>Blogs</h1>
+			{posts.map((post) => {
+				return <div key={post.id}>{post.title}</div>;
+			})}
+		</div>
+	);
 }
 export default ListPage;
