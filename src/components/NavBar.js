27f0f0b1b1f12d288b0b66896ccdd029299d205a
soohyuneee <tlstngus95@naver.com>
2023-04-17 19:06:03 +0900

    navlink 변경

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
 function NavBar() {
 	return (
@@ -9,9 +9,9 @@ function NavBar() {
 				</Link>
 				<ul className="navbar-nav">
 					<li className="nav-item">
-						<Link className="nav-link active" aria-current="page" to="/blogs">
+						<NavLink activeClassName="active" className="nav-link" aria-current="page" to="/blogs">
 							Blogs
-						</Link>
+						</NavLink>
 					</li>
 				</ul>
 			</div>
