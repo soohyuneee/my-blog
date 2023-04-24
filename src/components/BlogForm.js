0464bd057d67860749c59938d5bc13ee60617ab6
soohyuneee <tlstngus95@naver.com>
2023-04-24 17:04:50 +0900

    수정 기능 추가

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,40 +1,74 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
+import {bool} from 'prop-types';
 
-function BlogForm() {
+function BlogForm({editing}) {
 	const navigate = useNavigate();
+	const {id} = useParams();
+
 	const [title, setTitle] = useState('');
+	const [orginTitle, setOriginTitle] = useState('');
 	const [body, setBody] = useState('');
+	const [originBody, setOriginBody] = useState('');
+
+	useEffect(() => {
+		if (editing) {
+			axios.get(`http://localhost:3001/posts/${id}`).then((res) => {
+				setTitle(res.data.title);
+				setOriginTitle(res.data.title);
+				setBody(res.data.body);
+				setOriginBody(res.data.body);
+			});
+		}
+	}, [id, editing]);
+
+	const isEdited = () => {
+		return title !== orginTitle || body !== originBody;
+	};
 
 	const onSubmit = () => {
-		axios
-			.post('http://localhost:3001/posts', {
-				title,
-				body,
-				createdAt: Date.now(),
-			})
-			.then(() => {
-				navigate('/blogs');
+		if (editing) {
+			axios.patch(`http://localhost:3001/posts/${id}`, {title, body}).then(() => {
+				navigate(`/blogs/${id}`);
 			});
+		} else {
+			axios
+				.post('http://localhost:3001/posts', {
+					title,
+					body,
+					createdAt: Date.now(),
+				})
+				.then(() => {
+					navigate('/blogs');
+				});
+		}
 	};
 
 	return (
 		<div>
-			<h1>Create a blog post</h1>
+			<h1>{editing ? 'Edit' : 'Create'} a blog post</h1>
 			<div className="mb-3">
 				<label className="form-label">Title</label>
 				<input className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} />
 			</div>
 			<div className="mb-3">
 				<label className="form-label">Body</label>
-				<textarea className="form-control" value={body} onChange={(e) => setBody(e.target.value)} rows="20" />
+				<textarea className="form-control" value={body} onChange={(e) => setBody(e.target.value)} rows="10" />
 			</div>
-			<button className="btn btn-primary" onClick={onSubmit}>
-				Post
+			<button className="btn btn-primary" onClick={onSubmit} disabled={editing && !isEdited()}>
+				{editing ? 'Edit' : 'Post'}
 			</button>
 		</div>
 	);
 }
+
+BlogForm.propTypes = {
+	editing: bool,
+};
+
+BlogForm.defaultProps = {
+	editing: false,
+};
 
 export default BlogForm;
