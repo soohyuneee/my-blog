862836c66540f01ed2819f8f87ef213bfd86eb75
soohyuneee <tlstngus95@naver.com>
2023-04-25 23:20:00 +0900

    admin 버튼 추가

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,12 @@ function NavBar() {
 				<Link className="navbar-brand" to="/">
 					Home
 				</Link>
-				<ul className="navbar-nav">
+				<ul style={{flexDirection: 'row'}} className="navbar-nav">
+					<li className="nav-item me-2">
+						<NavLink activeclassname="active" className="nav-link" aria-current="page" to="/admin">
+							Admin
+						</NavLink>
+					</li>
 					<li className="nav-item">
 						<NavLink activeclassname="active" className="nav-link" aria-current="page" to="/blogs">
 							Blogs
