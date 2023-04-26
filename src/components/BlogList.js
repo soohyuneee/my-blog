8124da6bb57677afee4df3c13dc66ec519b8c24c
soohyuneee <tlstngus95@naver.com>
2023-04-26 17:48:58 +0900

    페이지네이션 추가

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -4,14 +4,36 @@ import Card from '../components/Card';
 import {useNavigate} from 'react-router-dom';
 import LoadingSpinner from '../components/LoadingSpinner';
 import {bool} from 'prop-types';
+import Pagination from './Pagination';
 
 function BlogList({isAdmin}) {
 	const navigate = useNavigate();
 	const [posts, setPosts] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [numberOfPosts, setNumberOfPosts] = useState(0);
+	const [numberOfPages, setNumberOfPages] = useState(0);
+	const limit = 5;
 
-	const getPosts = () => {
-		axios.get('http://localhost:3001/posts').then((res) => {
+	useEffect(() => {
+		setNumberOfPages(Math.ceil(numberOfPosts / limit));
+	}, [numberOfPosts]);
+
+	const getPosts = (page = 1) => {
+		setCurrentPage(page);
+		let params = {
+			_page: page,
+			_limit: limit,
+			_sort: 'id',
+			_order: 'desc',
+		};
+
+		if (!isAdmin) {
+			params = {...params, publish: true};
+		}
+
+		axios.get('http://localhost:3001/posts', {params}).then((res) => {
+			setNumberOfPosts(res.headers['x-total-count']);
 			setPosts(res.data);
 			setLoading(false);
 		});
@@ -37,9 +59,8 @@ function BlogList({isAdmin}) {
 		return <div>No blog posts found</div>;
 	}
 
-	return posts
-		.filter((post) => isAdmin || post.publish)
-		.map((post) => {
+	const rederBlogList = () => {
+		return posts.map((post) => {
 			return (
 				<Card key={post.id} title={post.title} onClick={() => navigate(`/blogs/${post.id}`)}>
 					{isAdmin ? (
@@ -52,6 +73,14 @@ function BlogList({isAdmin}) {
 				</Card>
 			);
 		});
+	};
+
+	return (
+		<div>
+			{rederBlogList()}
+			{numberOfPages > 1 && <Pagination currentPage={currentPage} numberOfPages={numberOfPages} onClick={getPosts} />}
+		</div>
+	);
 }
 
 BlogList.propTypes = {
---
+++ src/components/Pagination.js
@@ -0,0 +1,55 @@
+import propTypes from 'prop-types';
+
+function Pagination({currentPage, numberOfPages, onClick, limit}) {
+	const currentSet = Math.ceil(currentPage / limit);
+	const lastSet = Math.ceil(numberOfPages / limit);
+	const startPage = limit * (currentSet - 1) + 1;
+	const numberOfPageForSet = currentSet === lastSet ? numberOfPages % limit : limit;
+
+	return (
+		<nav aria-label="Page navigation example">
+			<ul className="pagination justify-content-center">
+				{currentSet !== 1 && (
+					<li className="page-item">
+						<div className="page-link cursor-pointer" onClick={() => onClick(startPage - limit)}>
+							Previous
+						</div>
+					</li>
+				)}
+				{Array(numberOfPageForSet)
+					.fill(startPage)
+					.map((value, index) => value + index)
+					.map((pageNumber) => {
+						return (
+							<li key={pageNumber} className={`page-item ${currentPage === pageNumber ? 'active' : ''}`}>
+								<div className="page-link cursor-pointer" onClick={() => onClick(pageNumber)}>
+									{pageNumber}
+								</div>
+							</li>
+						);
+					})}
+				{currentSet !== lastSet && (
+					<li className="page-item">
+						<div className="page-link cursor-pointer" onClick={() => onClick(startPage + limit)}>
+							Next
+						</div>
+					</li>
+				)}
+			</ul>
+		</nav>
+	);
+}
+
+Pagination.propTypes = {
+	currentPage: propTypes.number,
+	numberOfPages: propTypes.number.isRequired,
+	onClick: propTypes.func.isRequired,
+	limit: propTypes.number,
+};
+
+Pagination.defaultProps = {
+	currentPage: 1,
+	limit: 5,
+};
+
+export default Pagination;
