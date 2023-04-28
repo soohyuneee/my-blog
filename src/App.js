a0442109cb6ba873a1bb60f0727ffb05eb535f23
soohyuneee <tlstngus95@naver.com>
2023-04-29 00:58:46 +0900

    toast 전역관리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import routes from './routes';
+import Toast from './components/Toast';
+import useToast from './hooks/useToast';
+import {useSelector} from 'react-redux';
 
 function App() {
+	const {deleteToast} = useToast();
+	const toasts = useSelector((state) => state.toast.toasts);
+
 	return (
 		<BrowserRouter>
 			<NavBar />
+			<Toast toasts={toasts} deleteToast={deleteToast} />
 			<div className="container mt-3">
 				<Routes>
 					{routes.map((route) => {
