e2fecb1e04f1622cf955a387edc23e51eb6d0a18
soohyuneee <tlstngus95@naver.com>
2023-04-29 00:20:34 +0900

    토스트 추가

---
+++ src/components/Toast.js
@@ -0,0 +1,31 @@
+import propTypes from 'prop-types';
+
+function Toast({toasts, deleteToast}) {
+	return (
+		<div className="position-fixed bottom-0 end-0 p-2">
+			{toasts.map((toast) => {
+				return (
+					<div key={toast.id} onClick={() => deleteToast(toast.id)} className={`cursor-pointer alert alert-${toast.type || 'success'} m-0 py-2 mt-2`}>
+						{toast.text}
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
+Toast.propTypes = {
+	toasts: propTypes.arrayOf(
+		propTypes.shape({
+			text: propTypes.string,
+			type: propTypes.string,
+		}).isRequired
+	),
+	deleteToast: propTypes.func.isRequired,
+};
+
+Toast.defaultProps = {
+	toasts: [],
+};
+
+export default Toast;
