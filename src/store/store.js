5cc111c355bb6cd6909350476c77d5c4ed68efe0
soohyuneee <tlstngus95@naver.com>
2023-04-29 00:58:58 +0900

    toast 스토어 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
+import toastSlice from './toastSlice';
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		toast: toastSlice,
+	},
 });
