fb2151ec9fff7ab60288bb6a5aaeece234c210ff
soohyuneee <tlstngus95@naver.com>
2023-04-29 16:43:57 +0900

    auth 스토어 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import toastSlice from './toastSlice';
+import authSlice from './authSlice';
 
 export const store = configureStore({
 	reducer: {
 		toast: toastSlice,
+		auth: authSlice,
 	},
 });
