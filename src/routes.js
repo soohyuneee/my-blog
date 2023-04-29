85ca21f551a76cd32d1a9dbb3b97f2e556a45ec8
soohyuneee <tlstngus95@naver.com>
2023-04-29 16:43:13 +0900

    auth 속성, notfound 라우트 추가

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import CreatePage from './pages/CreatePage';
 import EditPage from './pages/EditPage';
 import ShowPage from './pages/ShowPage';
 import AdminPage from './pages/AdminPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const routes = [
 	{
@@ -17,18 +18,25 @@ const routes = [
 	{
 		path: '/admin',
 		component: <AdminPage />,
+		auth: true,
 	},
 	{
 		path: '/blogs/create',
 		component: <CreatePage />,
+		auth: true,
 	},
 	{
 		path: '/blogs/:id/edit',
 		component: <EditPage />,
+		auth: true,
 	},
 	{
 		path: '/blogs/:id',
 		component: <ShowPage />,
+	},
+	{
+		path: '*',
+		component: <NotFoundPage />,
 	},
 ];
 
