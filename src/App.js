47384923c28286f1f11378a7c2e13f78cc187216
soohyuneee <tlstngus95@naver.com>
2023-04-29 17:43:36 +0900

    리다이렉트, 로그인처리 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import NavBar from './components/NavBar';
 import routes from './routes';
 import Toast from './components/Toast';
 import useToast from './hooks/useToast';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import ProtectedRoute from './ProtectedRoute';
+import {useEffect, useState} from 'react';
+import {login} from './store/authSlice';
+import LoadingSpinner from './components/LoadingSpinner';
 
 function App() {
 	const {deleteToast} = useToast();
 	const toasts = useSelector((state) => state.toast.toasts);
+	const dispatch = useDispatch();
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		if (localStorage.getItem('isLoggedIn')) {
+			dispatch(login());
+		}
+		setLoading(false);
+	}, []);
+
+	if (loading) {
+		return <LoadingSpinner />;
+	}
 
 	return (
 		<BrowserRouter>
@@ -16,7 +33,7 @@ function App() {
 			<div className="container mt-3">
 				<Routes>
 					{routes.map((route) => {
-						return <Route key={route.path} path={route.path} element={route.component} />;
+						return <Route key={route.path} path={route.path} element={route.auth ? <ProtectedRoute element={route.element} /> : route.element} />;
 					})}
 				</Routes>
 			</div>
